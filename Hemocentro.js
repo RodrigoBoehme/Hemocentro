65a18638f45356de8a207b67479ad6acde2570a3
Shapognaty <robi.boehme@gmail.com>
2024-12-15 17:16:53 -0300

    Update Hemocentro.js

--- Hemocentro.js
+++ Hemocentro.js
@@ -1,212 +1,256 @@
-const readline=require('readline-sync')
+const readline = require('readline-sync')
 
-function formatDate(dDate,sMode){       
-        var today = dDate;
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
-        var yyyy = today.getFullYear();
-        if(dd<10) {
-            dd = '0'+dd
-        } 
-        if(mm<10) {
-            mm = '0'+mm
-        } 
-        if (sMode+""==""){
-            sMode = "dd/mm/yyyy";
-        }
-        if (sMode == "yyyy-mm-dd"){
-            return  yyyy + "-" + mm + "-" + dd + "";
-        }
-        if (sMode == "dd/mm/yyyy"){
-            return  dd + "/" + mm + "/" + yyyy;
-        }
-        if (sMode == "yyyymmdd"){
-            return yyyy+ mm + dd
-        }
+function formatDate(dDate, sMode) {
+    var today = dDate;
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    if (sMode + "" == "") {
+        sMode = "dd/mm/yyyy";
+    }
+    if (sMode == "yyyy-mm-dd") {
+        return yyyy + "-" + mm + "-" + dd + "";
+    }
+    if (sMode == "dd/mm/yyyy") {
+        return dd + "/" + mm + "/" + yyyy;
+    }
+    if (sMode == "yyyymmdd") {
+        return yyyy + mm + dd
+    }
 
 }
 
-function dataHue(input){
+function dataHue(input) {
 
-    if(input.includes('/')){
-        today=input.split('/')
-        dd=today[0]
-        mm=today[1]
-        yyyy=today[2]
+    if (input.includes('/')) {
+        today = input.split('/')
+        dd = today[0]
+        mm = today[1]
+        yyyy = today[2]
         return mm + '/' + dd + '/' + yyyy
     }
-    else if(input.includes('-')){
-        today=input.split('-')
-        dd=today[0]
-        mm=today[1]
-        yyyy=today[2]
-        return mm + '/' + dd + '/' + yyyy
-
-    }
-    else if(input.includes('|')){
-        let today=input.split('|')
-        dd=today[0]
-        mm=today[1]
-        yyyy=today[2]
+    else if (input.includes('-')) {
+        today = input.split('-')
+        dd = today[0]
+        mm = today[1]
+        yyyy = today[2]
         return mm + '/' + dd + '/' + yyyy
 
     }
+    else if (input.includes('|')) {
+        let today = input.split('|')
+        dd = today[0]
+        mm = today[1]
+        yyyy = today[2]
+        return mm + '/' + dd + '/' + yyyy
 
- //   let today=input.split('/'||'-'||'|')
- //   let dd=today[0]
- //   let mm=today[1]
- //   let yyyy=today[2]
-   }
+    }
 
-
-
-datas=[]
-
-
-i=0
-cadastroCliente=[]
-tipoSanguineo=['A','B','AB','O']//nome dos tipos
-antigenoD=['+','-']//se é positivo ou negativo
+    //   let today=input.split('/'||'-'||'|')
+    //   let dd=today[0]
+    //   let mm=today[1]
+    //   let yyyy=today[2]
+}
 
 
-function cadastro(){
-    paciente={}
-    paciente.id=i+1
+
+datas = []
+
+
+i = 0
+cadastroCliente = []
+tipoSanguineo = ['A', 'B', 'AB', 'O']//nome dos tipos
+antigenoD = ['+', '-']//se é positivo ou negativo
+
+
+function cadastro() {
+    paciente = {}
+    paciente.id = i + 1
 
     console.clear()
-    paciente.nome=readline.question(`Qual o Nome do paciente:`)
+    paciente.nome = readline.question(`Qual o Nome do paciente:`)
     console.clear()
 
-    paciente.tipoSanguineo=readline.questionInt((`Qual o Tipo Sanguineo do paciente?\n\n1. A\n2. B\n3. AB\n4. O\n\nDigite o numero correspondente: `))
+    paciente.tipoSanguineo = readline.questionInt((`Qual o Tipo Sanguineo do paciente?\n\n1. A\n2. B\n3. AB\n4. O\n\nDigite o numero correspondente: `))
     console.clear()
 
-    paciente.positividade=readline.questionInt('O sangue e positivo?\n\n1. Sim\n2. Nao\n\nDigite o Numero correspondente: ')
+    if (paciente.tipoSanguineo > 4 || paciente.tipoSanguineo < 1) {
+        readline.question(`Opcao Invalida...`)
+        return
+    }
+
+    paciente.positividade = readline.questionInt('O sangue e positivo?\n\n1. Sim\n2. Nao\n\nDigite o Numero correspondente: ')
     console.clear()
 
-    const dataISO= new Date(dataHue(readline.question(`Informe a data: `)))
-   // formatDate(dataISO,'dd/mm/yyyy')
-    paciente.data=dataISO
+    if (paciente.positividade > 2 || paciente.positividade < 1) {
+        readline.question(`Opcao Invalida...`)
+        return
+    }
+
+    const dataISO = new Date(dataHue(readline.question(`Informe a data: `)))
+    // formatDate(dataISO,'dd/mm/yyyy')
+    paciente.data = dataISO
+    if (paciente.data == 'Invalid Date') {
+        paciente.data = new Date
+    }
     cadastroCliente.push(paciente)
     //console.log(cadastroCliente[i])
-    
+
     i++
 }
 
-function verPaciente(){
-    console.clear
-    console.log('-------------------------------------------------------------------------------------------------------------------')
-    console.log(`| IDs    | Nome                          |Data Cadastro         | Tipo Sanguineo                                 | `)
-    console.log('-------------------------------------------------------------------------------------------------------------------')
-    for(let a=0;a<cadastroCliente.length;a++){
-        
-        console.log(`| ID: ${(cadastroCliente[a].id).toString().padEnd(3)}| ${(cadastroCliente[a].nome).padEnd(30)}| ${formatDate(cadastroCliente[a].data,'dd/mm/yyyy').padEnd(21)}| ${(tipoSanguineo[cadastroCliente[a].tipoSanguineo-1]+antigenoD[cadastroCliente[a].positividade-1]).padEnd(48
+function verPaciente() {
+    if (cadastroCliente.length > 0) {
+        console.clear
+        console.log('-------------------------------------------------------------------------------------------------------------------')
+        console.log(`| IDs    | Nome                          |Data Cadastro         | Tipo Sanguineo                                 | `)
+        console.log('------------------------------------------------------------------------------------------------------------------')
+        for (let a = 0; a < cadastroCliente.length; a++) {
+
+            console.log(`|  ${(cadastroCliente[a].id).toString().padEnd(6)}| ${(cadastroCliente[a].nome).padEnd(30)}| ${formatDate(cadastroCliente[a].data, 'dd/mm/yyyy').padEnd(21)}| ${(tipoSanguineo[cadastroCliente[a].tipoSanguineo - 1] + antigenoD[cadastroCliente[a].positividade - 1]).padEnd(47
             )}|`)
+        }
+        console.log('------------------------------------------------------------------------------------------------------------------')
+    } else {
+        console.log(`Nao ha pessoas registradas`)
     }
-    console.log('-------------------------------------------------------------------------------------------------------------------')
 }
-function pacienteTipoSangue(){
-   arrayTipo=[]
-   arrayAntigeno=[]
+
+function pacienteTipoSangue() {
+    arrayTipo = []
+    arrayAntigeno = []
     let opcaoTipo;
     let opcaoAntingeno;
 
-    console.log(`Escolha um tipo de Sangue:`)
-            for(let i=0;i<tipoSanguineo.length;i++){
-            console.log(`${i+1}. ${tipoSanguineo[i]}`)
+    if (cadastroCliente.length > 0) {
+        console.log(`Escolha um tipo de Sangue:`)
+        for (let i = 0; i < tipoSanguineo.length; i++) {
+            console.log(`${i + 1}. ${tipoSanguineo[i]}`)
+        }
+        opcaoTipo = readline.questionInt(`Escolha um Numero: `)
+        console.clear()
+        if (opcaoTipo > 4 || opcaoTipo < 0) {
+            readline.question(`Opcao Invalida...`)
+            return
+        }
+
+        opcaoAntingeno = readline.questionInt('Positivo ou Negativo?\n\n1. Positivo\n2. Negativo\nEscolha um numero: ')
+        console.clear()
+        if (opcaoAntingeno > 2 || opcaoAntingeno < 0) {
+            readline.question(`Opcao Invalida...`)
+            return
+        }
+
+
+        for (let i = 0; i < cadastroCliente.length; i++) {
+            if (cadastroCliente[i].positividade == opcaoAntingeno) {
+                arrayAntigeno.push(cadastroCliente[i])
             }
-        opcaoTipo=readline.questionInt(`Escolha um Numero: `)
-
-        opcaoAntingeno=readline.questionInt('Positivo ou Negativo?\n\n1. Positivo\n2. Negativo\nEscolha um numero: ')
-        
-
-        for(let i=0;i<cadastroCliente.length;i++){
-        if(cadastroCliente[i].positividade==opcaoAntingeno){
-            arrayAntigeno.push(cadastroCliente[i])
-            20231231
         }
-} 
-     for(let i=0;i<arrayAntigeno.length;i++){
-        if(arrayAntigeno[i].tipoSanguineo==opcaoTipo){
-            arrayTipo.push(arrayAntigeno[i])
+        for (let i = 0; i < arrayAntigeno.length; i++) {
+            if (arrayAntigeno[i].tipoSanguineo == opcaoTipo) {
+                arrayTipo.push(arrayAntigeno[i])
+            }
         }
-}
-    if(arrayTipo.length>0){
-    console.clear()
-    console.log(`Pessoas com o sangue ${tipoSanguineo[opcaoTipo-1]}${antigenoD[opcaoAntingeno-1]}`)
+        if (arrayTipo.length > 0) {
+            console.clear()
+            console.log(`Pessoas com o sangue ${tipoSanguineo[opcaoTipo - 1]}${antigenoD[opcaoAntingeno - 1]}`)
+            console.log('-------------------------------------------------------------------------------------------------------------------')
+            for (let a = 0; a < arrayTipo.length; a++) {
+                // console.log(`${i+1}. ${arrayTipo[i].nome} Data cadastro:${formatDate(cadastroCliente[i].data,'dd/mm/yyyy')}, Tipo Sanguineo:${tipoSanguineo[cadastroCliente[i].tipoSanguineo-1]}${antigenoD[cadastroCliente[i].positividade-1]}`)
 
-    console.log('-------------------------------------------------------------------------------------------------------------------')
-        for(let a=0;a<arrayTipo.length;a++){
-           // console.log(`${i+1}. ${arrayTipo[i].nome} Data cadastro:${formatDate(cadastroCliente[i].data,'dd/mm/yyyy')}, Tipo Sanguineo:${tipoSanguineo[cadastroCliente[i].tipoSanguineo-1]}${antigenoD[cadastroCliente[i].positividade-1]}`)
-
-            console.log(`| ID: ${(cadastroCliente[a].id).toString().padEnd(3)}| Nome:${(cadastroCliente[a].nome).padEnd(30)}| Data cadastro:${formatDate(cadastroCliente[a].data,'dd/mm/yyyy').padEnd(15)}| Tipo Sanguineo:${(tipoSanguineo[cadastroCliente[a].tipoSanguineo-1]+antigenoD[cadastroCliente[a].positividade-1]).padEnd(20)}|`)
+                console.log(`| ID: ${(cadastroCliente[a].id).toString().padEnd(3)}| Nome:${(cadastroCliente[a].nome).padEnd(30)}| Data cadastro:${formatDate(cadastroCliente[a].data, 'dd/mm/yyyy').padEnd(15)}| Tipo Sanguineo:${(tipoSanguineo[cadastroCliente[a].tipoSanguineo - 1] + antigenoD[cadastroCliente[a].positividade - 1]).padEnd(20)}|`)
+            }
+            console.log('-------------------------------------------------------------------------------------------------------------------')
+        } else {
+            console.log('-------------------------------------------------------------------------------------------------------------------')
+            console.log(`Nao ha pessoas cadastradas com o sangue especificado`)
+            console.log('-------------------------------------------------------------------------------------------------------------------')
         }
-        console.log('-------------------------------------------------------------------------------------------------------------------')
-
-}
-}
-function pesquisarData(){
-    let nSeiAinda=new Date(dataHue(readline.question(`Qual a data a ser buscada? :`)))
-    let numeroTransformado= Number(formatDate(nSeiAinda,"yyyymmdd"))
-    console.log(numeroTransformado)
-    console.log('-------------------------------------------------------------------------------------------------------------------')
-    for(let a=0;a<cadastroCliente.length;a++){
-        if(Number(formatDate(cadastroCliente[a].data,"yyyymmdd"))<=numeroTransformado){
-            console.log(`| ID: ${(cadastroCliente[a].id).toString().padEnd(3)}| Nome:${(cadastroCliente[a].nome).padEnd(30)}| Data cadastro:${formatDate(cadastroCliente[a].data,'dd/mm/yyyy').padEnd(15)}| Tipo Sanguineo:${(tipoSanguineo[cadastroCliente[a].tipoSanguineo-1]+antigenoD[cadastroCliente[a].positividade-1]).padEnd(20)}|`)
-        
-        }
-        
+    } else {
+        console.log(`Nao ha pessoas cadastradas`)
     }
-    console.log('-------------------------------------------------------------------------------------------------------------------')
+}
+function pesquisarData() {
+    if (cadastroCliente.length > 0) {
+        arrayData = []
+        let nSeiAinda = new Date(dataHue(readline.question(`Qual a data a ser buscada? :`)))
+        let numeroTransformado = Number(formatDate(nSeiAinda, "yyyymmdd"))
+        //    console.log(numeroTransformado)
+        console.log('-------------------------------------------------------------------------------------------------------------------')
+        for (let a = 0; a < cadastroCliente.length; a++) {
+            if (Number(formatDate(cadastroCliente[a].data, "yyyymmdd")) <= numeroTransformado) {
+                arrayData.push(cadastroCliente[a])
+            }
+        }
+        if (arrayData.length > 0) {
+
+            console.log('-------------------------------------------------------------------------------------------------------------------')
+            for (let a = 0; a < arrayData.length; a++) {
+                console.log(`| ID: ${(arrayData[a].id).toString().padEnd(3)}| Nome:${(arrayData[a].nome).padEnd(30)}| Data cadastro:${formatDate(arrayData[a].data, 'dd/mm/yyyy').padEnd(15)}| Tipo Sanguineo:${(tipoSanguineo[arrayData[a].tipoSanguineo - 1] + antigenoD[arrayData[a].positividade - 1]).padEnd(20)}|`)
+            }
+            console.log('-------------------------------------------------------------------------------------------------------------------')
+        }
+    }
+    else {
+        console.log(`Nao ha pessoas cadastradas`)
+    }
 }
 
 
 
 
 
-function menuHemocentro(){
-    while(true){
-        opcao=readline.questionInt('\===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====\n1. Cadastrar doador\n2. Listar doadores\n3. Buscar doador por tipo sanguineo\n4. Buscar doador por data da ultima doacao\n5. Sair\n\nDigite o Numero: ')
+function menuHemocentro() {
+    while (true) {
+        opcao = readline.questionInt('\===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====\n1. Cadastrar doador\n2. Listar doadores\n3. Buscar doador por tipo sanguineo\n4. Buscar doador por data da ultima doacao\n5. Sair\n\nDigite o Numero: ')
+        console.clear()
 
-        switch(opcao){
+        switch (opcao) {
             case 1:
                 cadastro()
                 console.clear()
                 readline.question('Pressione Enter para continuar')
                 console.clear
-            break;
+                break;
 
             case 2:
                 verPaciente()
                 readline.question('Pressione Enter para continuar')
                 console.clear()
-            break;
+                break;
 
             case 3:
                 pacienteTipoSangue()
                 readline.question('Pressione Enter para continuar')
                 console.clear()
-            break;
+                break;
 
             case 4:
-                pesquisarData()                
+                pesquisarData()
                 readline.question('Pressione Enter para continuar')
                 console.clear()
-            break;
+                break;
 
             case 5:
-
                 console.clear()
-            return;
+                return;
 
             default:
 
-            break;
+                break;
 
             case 27092002:
                 console.log('Essa opcao e um easter egg')
-            break;
+                break;
         }
     }
 }
-
+console.clear()
 menuHemocentro()
